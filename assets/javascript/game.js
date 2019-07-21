aa1bff0603e2ebb1203dd9aaf181f5a5c844ad24
LMBoyle <boylelukem@gmail.com>
2019-07-21 17:26:34 -0400

    pseudocoded javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,42 @@
+// VARIABLES
+// ==============================================
+// CHOOSE YOUR FIGHTER
+// On click, choose a character
+
+// Show fight arena
+
+// Hide character select
+
+// Show character under "fighter"
+
+// Show other characters under "enemiesLeft"
+
+// ==============================================
+// CHOOSE AN ENEMY
+// On click, choose an enemy
+
+// Show enemy under "defender"
+
+// If enemy health = 0, show greyed out or with an X, and make unclickable
+
+// ==============================================
+// FIGHT
+// On button click
+
+// Take health from enemy based on user attack value
+
+// Take health from user based on enemy counter attack value
+
+// Add base attack value to new attack value of user
+
+// Loop until one health is 0 or below
+
+// ==============================================
+// WIN/LOSE
+// If defeated enemy, loop to choose an enemy
+
+// If lose, loop to reset
+
+// ==============================================
+// RESET
+// ==============================================
