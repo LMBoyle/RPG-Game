fdeeada14196f9cdef4528267b3052bb3c0bddb0
LMBoyle <boylelukem@gmail.com>
2019-07-22 14:17:52 -0400

    after character is selected, other characters go to enemies available to attack

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,34 @@ var characters = ["assets/images/Obi-wan.png", "assets/images/Luke.jpg", "assets
 // CHOOSE YOUR FIGHTER
 function startGame() {
 // On click, choose a character
+document.addEventListener ('click', function(img) {
+  if (img.target.id === "obi") {
+    // Show character under "user"
+    document.getElementById("user").src = characters[0];
+    // Show other characters under "enemiesLeft"
+    document.getElementById("one").src = characters[1];
+    document.getElementById("two").src = characters[2];
+    document.getElementById("three").src = characters[3];
+  }
+  else if (img.target.id === "luke") {
+    document.getElementById("user").src = characters[1];
+    document.getElementById("one").src = characters[0];
+    document.getElementById("two").src = characters[2];
+    document.getElementById("three").src = characters[3];
+  }
+  else if (img.target.id === "sid") {
+    document.getElementById("user").src = characters[2];
+    document.getElementById("one").src = characters[0];
+    document.getElementById("two").src = characters[1];
+    document.getElementById("three").src = characters[3];
+  }
+  else if (img.target.id === "maul") {
+    document.getElementById("user").src = characters[3];
+    document.getElementById("one").src = characters[0];
+    document.getElementById("two").src = characters[1];
+    document.getElementById("three").src = characters[2];
+  }
+})
 
 // Show fight arena
   var a = document.getElementById("showHide");
@@ -26,26 +54,8 @@ function startGame() {
     f.style.display = "grid";
   }
 
-// Show character under "user"
-document.addEventListener ('click', function(img) {
-console.log('event:', img.target.id);
-  if (img.target.id === "obi") {
-    document.getElementById("user").src = characters[0];
-  }
-  else if (img.target.id === "luke") {
-    document.getElementById("user").src = characters[1];
-  }
-  else if (img.target.id === "sid") {
-    document.getElementById("user").src = characters[2];
-  }
-  else {
-    document.getElementById("user").src = characters[3];
-  }
-})
-  // if (event === )
 
-// 
-// Show other characters under "enemiesLeft"
+
 }
 // ==============================================
 // CHOOSE AN ENEMY
