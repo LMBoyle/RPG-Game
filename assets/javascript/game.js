0b45cde8398121e34e3c4184366024594d5bbeca
LMBoyle <boylelukem@gmail.com>
2019-07-21 21:40:58 -0400

    added onclick to hide the game until a fighter is choosen, then hide the fighter selection and show the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,42 @@
 // VARIABLES
+
 // ==============================================
 // CHOOSE YOUR FIGHTER
+function startGame() {
 // On click, choose a character
 
 // Show fight arena
+console.log("You clicked me!");
+
+  var a = document.getElementById("showHide");
+
+  if (a.style.display === "none") {
+    a.style.display = "grid";
+  } 
+  else {
+    a.style.display = "none";
+  }
 
 // Hide character select
+  var f = document.getElementById("choose");
 
+  if (f.style.display === "grid") {
+    f.style.display = "none";
+  }
+  else {
+    f.style.display = "grid";
+  }
 // Show character under "fighter"
 
 // Show other characters under "enemiesLeft"
-
+}
 // ==============================================
 // CHOOSE AN ENEMY
 // On click, choose an enemy
 
 // Show enemy under "defender"
 
-// If enemy health = 0, show greyed out or with an X, and make unclickable
+// If enemy health = 0, show greyed out or with an v, and make unclickable
 
 // ==============================================
 // FIGHT
