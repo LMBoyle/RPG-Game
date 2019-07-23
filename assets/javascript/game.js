ef622ea63df55cd01af93f18cc7a407c39671ea9
LMBoyle <boylelukem@gmail.com>
2019-07-23 15:41:15 -0400

    changed character selection to jqery and added some css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,76 @@
 // VARIABLES
-var characters = ["assets/images/Obi-wan.png", "assets/images/Luke.jpg", "assets/images/Darth_Sidious.jpg", "assets/images/Darth_Maul.png"];
-var user = document.getElementById("user");
-var one = document.getElementById("one");
-var two = document.getElementById("two");
-var three = document.getElementById("three");
+var obi = {
+  name: "Obi-Wan Kenobi",
+  image: "assets/images/Obi-wan.png",
+  lightsaber: {"box-shadow" : "0 0 8px 3px rgb(148, 167, 231)"},
+  health: 120,
+  attack: 8,
+  counterAttack: 10,
+}
+var luke = {
+  name: "Luke Skywalker",
+  image: "assets/images/Luke.jpg",
+  lightsaber: {"box-shadow" : "0 0 8px 3px green"},
+  health: 100,
+  attack: 10,
+  counterAttack: 15,
+}
+var sidious = {
+  name: "Darth Sidious",
+  image: "assets/images/Darth_Sidious.jpg",
+  lightsaber: {"box-shadow" : "0 0 8px 3px red"},
+  health: 150,
+  attack: 15,
+  counterAttack: 20,
+}
+var maul = {
+  name: "Darth Maul",
+  image: "assets/images/Darth_Maul.png",
+  lightsaber: {"box-shadow" : "0 0 8px 3px red"},
+  health: 110,
+  attack: 20,
+  counterAttack: 25,
+}
+var user = $("#user");
+var one = $("<img>");
+var two = $("<img>");
+var three = $("<img>");
 // ==============================================
 // CHOOSE YOUR FIGHTER
 // On click, choose a character
-$(".choose").click(function(img) {
-
-  console.log("you clicked me")
-
+function startGame(img) {
 
   if (img.target.id === "obi") {
     // Show character under "user"
-    user.src = characters[0];
+    user.attr("src", obi.image).attr("alt", obi.name).css(obi.lightsaber);
     // Show other characters under "enemiesLeft"
-    one.src = characters[1];
-    two.src = characters[2];
-    three.src = characters[3];
+    one.attr("id", "one").attr("src", luke.image).attr("alt", luke.name).css(luke.lightsaber).appendTo("#enemy1");
+    two.attr("id", "two").attr("src", sidious.image).attr("alt", sidious.name).css(sidious.lightsaber).appendTo("#enemy2");
+    three.attr("id", "three").attr("src", maul.image).attr("alt", maul.name).css(maul.lightsaber).appendTo("#enemy3");
   }
   else if (img.target.id === "luke") {
-    user.src = characters[1];
-    one.src = characters[0];
-    two.src = characters[2];
-    three.src = characters[3];
+    // Show character under "user"
+    user.attr("src", luke.image).attr("alt", luke.name).css(luke.lightsaber);
+    // Show other characters under "enemiesLeft"
+    one.attr("id", "one").attr("src", obi.image).attr("alt", obi.name).css(obi.lightsaber).appendTo("#enemy1");
+    two.attr("id", "two").attr("src", sidious.image).attr("alt", sidious.name).css(sidious.lightsaber).appendTo("#enemy2");
+    three.attr("id", "three").attr("src", maul.image).attr("alt", maul.name).css(maul.lightsaber).appendTo("#enemy3");
   }
   else if (img.target.id === "sid") {
-    user.src = characters[2];
-    one.src = characters[0];
-    two.src = characters[1];
-    three.src = characters[3];
+    // Show character under "user"
+    user.attr("src", sidious.image).attr("alt", sidious.name).css(sidious.lightsaber);
+    // Show other characters under "enemiesLeft"
+    one.attr("id", "one").attr("src", obi.image).attr("alt", obi.name).css(obi.lightsaber).appendTo("#enemy1");
+    two.attr("id", "two").attr("src", luke.image).attr("alt", luke.name).css(luke.lightsaber).appendTo("#enemy2");
+    three.attr("id", "three").attr("src", maul.image).attr("alt", maul.name).css(maul.lightsaber).appendTo("#enemy3");
   }
   else if (img.target.id === "maul") {
-    user.src = characters[3];
-    one.src = characters[0];
-    two.src = characters[1];
-    three.src = characters[2];
+    // Show character under "user"
+    user.attr("src", maul.image).attr("alt", maul.name).css(maul.lightsaber);
+    // Show other characters under "enemiesLeft"
+    one.attr("id", "one").attr("src", obi.image).attr("alt", obi.name).css(obi.lightsaber).appendTo("#enemy1");
+    two.attr("id", "two").attr("src", luke.image).attr("alt", luke.name).css(luke.lightsaber).appendTo("#enemy2");
+    three.attr("id", "three").attr("src", sidious.image).attr("alt", sidious.name).css(sidious.lightsaber).appendTo("#enemy3");
   }
 
 
@@ -45,31 +79,7 @@ $(".choose").click(function(img) {
 
 // Hide character select
   $(".charSelect").toggle();
-
-/*
-  var a = document.getElementById("showHide");
-
-  if (a.style.display === "none") {
-    a.style.display = "grid";
-  } 
-  else {
-    a.style.display = "none";
-  }
-*/
-
-
-
-/*
-  var f = document.getElementById("choose");
-
-  if (f.style.display === "grid") {
-    f.style.display = "none";
-  }
-  else {
-    f.style.display = "grid";
-  }
-*/
-})
+}
 
 
 // ==============================================
@@ -101,3 +111,7 @@ $(".choose").click(function(img) {
 // ==============================================
 // RESET
 // ==============================================
+// CALL FUNCTIONS
+$(document).ready(function() {
+  $(".choose").click(startGame);
+});
